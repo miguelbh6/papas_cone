67876f137dc74612a7807493741da9966c4bc954
Francisco Miguel <miguelbh6@gmail.com>
2018-08-22 00:35:32 -0300

    Alteracao em Footer

--- application/views/site/footer.php
+++ application/views/site/footer.php
@@ -47,7 +47,7 @@
             <div class="media-container-row mbr-white">
                 <div class="col-sm-6 copyright">
                     <p class="mbr-text mbr-fonts-style display-7">
-                        © Copyright 2017 Papas Cone - All Rights Reserved
+                        © Copyright 2017 Papas Cone! - All Rights Reserved
                     </p>
                 </div>
                 <div class="col-md-6">
